db502b63ece38f6f1c68b4b539213ae4b019a5d6
Jambolot <darcklockgosha@gmail.com>
2024-07-23 01:25:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace homework25;
+
+class Program
+{
+    static void Main()
+    {
+        MyLinkedList<int> list = new MyLinkedList<int>();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        Console.WriteLine("Count: " + list.Count);
+        Console.WriteLine("First: " + list.First.Value);
+        Console.WriteLine("Last: " + list.Last.Value);
+    }
+}
